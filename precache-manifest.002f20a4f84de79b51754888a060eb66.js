97cdf763e0ab8cb4a3d77637f82ab24efeeb21f1
Filip Sutkowy <filip.sutkowy@gmail.com>
2019-10-08 00:46:32 +0200

    Updates

---
+++ precache-manifest.002f20a4f84de79b51754888a060eb66.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72d6245836d95707b838b8f654d63b9d",
+    "url": "/burgerapp/index.html"
+  },
+  {
+    "revision": "b9639d1766f2f3e4ba36",
+    "url": "/burgerapp/static/css/3.bef337ad.chunk.css"
+  },
+  {
+    "revision": "b0fe76deb7a54247d27d",
+    "url": "/burgerapp/static/css/4.f4473488.chunk.css"
+  },
+  {
+    "revision": "897c35dcee874fc1e6e8",
+    "url": "/burgerapp/static/css/5.0bc05d0c.chunk.css"
+  },
+  {
+    "revision": "f4cbc2db716702d0e0c9",
+    "url": "/burgerapp/static/css/main.09c5880f.chunk.css"
+  },
+  {
+    "revision": "f4d983fc9f6e158ac127",
+    "url": "/burgerapp/static/js/2.01bb6e49.chunk.js"
+  },
+  {
+    "revision": "b9639d1766f2f3e4ba36",
+    "url": "/burgerapp/static/js/3.4059c6b4.chunk.js"
+  },
+  {
+    "revision": "b0fe76deb7a54247d27d",
+    "url": "/burgerapp/static/js/4.b4a0cf6c.chunk.js"
+  },
+  {
+    "revision": "897c35dcee874fc1e6e8",
+    "url": "/burgerapp/static/js/5.75836ebf.chunk.js"
+  },
+  {
+    "revision": "f4cbc2db716702d0e0c9",
+    "url": "/burgerapp/static/js/main.12c16fd5.chunk.js"
+  },
+  {
+    "revision": "f348339523657d6e1aad",
+    "url": "/burgerapp/static/js/runtime~main.a9e37ed2.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burgerapp/static/media/burger-logo.b8503d26.png"
+  }
+]);
